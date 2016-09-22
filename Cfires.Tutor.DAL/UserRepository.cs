4dd9d6333378a31f7448bdd299b1286c8e83d2b3
cfires <caofangzh@163.com>
2016-09-22 13:15:55 +0800

    Update UserRepository.cs

--- Cfires.Tutor.DAL/UserRepository.cs
+++ Cfires.Tutor.DAL/UserRepository.cs
@@ -1,6 +1,5 @@
-﻿using Cfires.Tutor.Model;
+using Cfires.Tutor.Model;
 using NPoco;
-using System.Configuration;
 using System;
 using System.Data;
 using System.Data.SqlClient;
@@ -8,15 +7,9 @@ using System.Collections.Generic;
 
 namespace Cfires.Tutor.DAL
 {
-    public class UserRepository
+    public class UserRepository : Repository<Base_User>
     {
         IDatabase db = new Database("connStr");
-
-        public Base_User GetById(int id)
-        {
-            var sql = Sql.Builder.Append("SELECT * FROM Base_User WHERE Id = @0", id);
-            return db.FirstOrDefault<Base_User>(sql);
-        }
 
         public Base_User GetByEmail(string email)
         {
@@ -24,16 +17,38 @@ namespace Cfires.Tutor.DAL
             return db.FirstOrDefault<Base_User>(sql);
         }
 
-        public void Insert(Base_User user)
-        {
-            db.Insert<Base_User>(user);
-        }
-
         #region 用户管理
 
-        public List<Base_User> GetUserList()
+        /// <summary>
+        /// 获取用户集合
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <returns></returns>
+        public List<Base_User> GetList(int start, int end)
         {
-            return null;
+            string sql = "SELECT * FROM (SELECT row_number() over(order by Id) as num,* from Base_User) as t where t.num >=@start and t.num <=@end";
+
+            SqlParameter[] pars = {
+                new SqlParameter("@start",start),
+                new SqlParameter("@end",end)
+            };
+            List<Base_User> list = new List<Base_User>();
+            DataTable dt = SqlHelper.ExecuteDataTable(sql, pars);
+
+            foreach (DataRow row in dt.Rows)
+            {
+                list.Add(new Base_User()
+                {
+                    Id = Convert.ToInt32(row["Id"]),
+                    Name = row["Name"].ToString(),
+                    Password = row["Password"].ToString(),
+                    Email = row["Email"].ToString(),
+                    Type = (UserType)row["Type"],
+                    CreateDate = (DateTime)row["CreateDate"]
+                });
+            }
+            return list;
         }
         #endregion
     }
